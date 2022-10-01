ca3147750cafe2948612eca6753f8433c2dcba53
HelithaSri <helitha.pravin@gmail.com>
2022-10-02 00:36:02 +0530

    Redesign ui

--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,16 @@
-import { View,StyleSheet } from 'react-native'
+import { View,StyleSheet, Image } from 'react-native'
 import React from 'react'
 import { NativeBaseProvider, Box, Button, Switch, VStack, TextArea,Heading, Text, Input } from "native-base";
-
 export default function Login() {
   return (
     <NativeBaseProvider>
       <VStack style={style.container} space={5}>
-      <Heading style={style.heading} size="xl">Login</Heading>
-      <Input mx="3" placeholder="Email" w="80%" />
-
-      <Input mx="3" placeholder="Password" w="80%" />
-      
+      <Heading style={style.heading} size="xl">WELCOME BACK</Heading>
+      <Image style={{marginBottom:'10%',}} source={require("../assets/img/login.png")} />
+      <Input mx="3" placeholder="Email" w="80%"/>
+      <Input mx="3" placeholder="Password" w="80%"/>
       <Button style={style.btn} onPress={() => console.log("hello world")} w="50%">Login</Button>
+      <Text style={{marginTop:'15%',}}>Don’t have  an account? Register</Text>
       </VStack>
     </NativeBaseProvider>
   );
@@ -21,14 +20,15 @@ const style = StyleSheet.create({
   container:{
     justifyContent:'center',
     alignItems:'center',
-    marginTop: '20%',
+    marginTop: '10%',
    },
 
    heading:{
-    marginBottom: '10%'
+    marginBottom: 20
    },
 
    btn:{
-    backgroundColor:"#0984e3"
-   }
+    backgroundColor:"#469AFF",
+    
+   },
 })
