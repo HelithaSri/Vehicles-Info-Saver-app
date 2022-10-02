d3d136f986e4214b1c52102f95e8e47b088240b9
HelithaSri <helitha.pravin@gmail.com>
2022-10-03 01:50:26 +0530

    Change Icon color

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import Register from './screens/Register';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Home from './screens/Home';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -29,12 +31,31 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
+{/* <Home/> */}
 function Root() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen options={{ headerShown: false }} name="Login" component={Login} />
-      <Tab.Screen options={{ headerShown: false }} name="Register" component={Register} />
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Add') {
+              iconName = focused ? 'create' : 'create-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#469AFF',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+      <Tab.Screen options={{ headerShown: false }} name="Home" component={Home} />
+      <Tab.Screen options={{ headerShown: false }} name="Add" component={Register} />
     </Tab.Navigator>
   );
 }
