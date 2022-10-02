7a579e261eb99a2954773fba44ad62ba634e9d95
HelithaSri <helitha.pravin@gmail.com>
2022-10-02 15:06:16 +0530

    Wrapp app using NavigationContainer

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
 import Login from './screens/Login';
+import Register from './screens/Register';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-      <Login/>
-  )
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
