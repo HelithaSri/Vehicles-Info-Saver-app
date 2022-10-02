33ab3e55ed5e8a60e51bc9082f394c6cc100296e
HelithaSri <helitha.pravin@gmail.com>
2022-10-02 15:29:08 +0530

    Nesting navigator create

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import Login from './screens/Login';
 import Register from './screens/Register';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -16,7 +18,23 @@ export default function App() {
         }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen
+          name="Root"
+          component={Root}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
+
+      
     </NavigationContainer>
+  );
+}
+
+function Root() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen options={{ headerShown: false }} name="Login" component={Login} />
+      <Tab.Screen options={{ headerShown: false }} name="Register" component={Register} />
+    </Tab.Navigator>
   );
 }
